76d8bc58410fc9ea231a080ba4d4796f985cc964
Byron Zhang <xiaoqi_2591@outlook.com>
2016-09-13 10:03:04 +0800

    redis: fix wrong volume config

--- redis/start.sh
+++ redis/start.sh
@@ -4,9 +4,14 @@ set -e
 exec 2>&1
 
 configFile=/etc/redis/redis.conf
+logpath=/var/log/redis
+dbpath=/var/lib/redis
 
 if [ $REDIS_PASSWORD ]; then
   echo "requirepass $REDIS_PASSWORD" >> $configFile
 fi
+
+chown -R redis $logpath
+chown -R redis $dbpath
 
 /sbin/setuser redis redis-server $configFile
