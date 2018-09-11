a430178a77ec42778afe39d68d2fb5a3d702b6b6
Byron Zhang <byronzhang@augmentum.com.cn>
2018-09-11 17:25:28 +0800

    add docker playgroud file

---
+++ playground/ns.c
@@ -0,0 +1,41 @@
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sched.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/mount.h>
+
+/* 定义一个给 clone 用的栈，栈大小1M */
+#define STACK_SIZE (1024 * 1024)
+static char container_stack[STACK_SIZE];
+
+char* const container_args[] = {
+    "/bin/bash",
+    NULL
+};
+
+int container_main(void* arg)
+{
+    printf("Container - inside the container!\n");
+    // 如果你的机器的要目录的挂载类型是 shared, 那必须先重新挂载要目录
+    mount("", "/", NULL, MS_PRIVATE, "");
+    mount("none", "/tmp", "tmpfs", 0, "");
+    /* 直接执行一个shell，以便我们观察这个进程空间里的资源是否被隔离了 */
+    execv(container_args[0], container_args);
+    printf("Something's wrong!\n");
+    return 1;
+}
+
+int main()
+{
+    printf("Parent - start a container!\n");
+    /* 调用clone函数，其中传出一个函数，还有一个栈空间的（为什么传尾指针，因为栈是反着的） */
+    int container_pid = clone(container_main, container_stack+STACK_SIZE,
+        CLONE_NEWNS | CLONE_NEWPID | SIGCHLD, NULL);
+    /* 等待子进程结束 */
+    waitpid(container_pid, NULL, 0);
+    printf("Parent - container stopped!\n");
+    return 0;
+}
